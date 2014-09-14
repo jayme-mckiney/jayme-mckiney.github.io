7c2832f951e1a412926b1a64b7e1309cb25c28fe
thesock <jayme.mckiney@gmail.com>
2014-09-14 15:32:31 -0700

    made nav bar break out of header and follow screen

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,19 +1,28 @@
 $(document).ready( function() {
-    $.ajax({
-    url: "http://jayme-mckiney.github.io/nav_menu.html",
-    cache: false
-  }).done(function( html ) {
-    $('nav').html(html);
-    $('.menu').hover(function() {
-      var subMenu = $(this).find('> ul');
-      if ($(this).find('> a').hasClass('collapsed')) {
-        subMenu.fadeIn();
-      } else {
-        subMenu.fadeOut();
-      }
-      $(this).find('> a').toggleClass('collapsed').toggleClass('expanded');
-    });
+  $(window).scroll(function() {
+    var height = $(window).scrollTop();
+    var position = parseInt($("header").css('font-size')) * 6.5;
+    if(height  > position) {
+      $('nav').addClass('fixednav');
+    } else {
+      $('nav').removeClass('fixednav');
+    }
   });
+  $.ajax({
+  url: "http://jayme-mckiney.github.io/nav_menu.html",
+  cache: false
+}).done(function( html ) {
+  $('nav').html(html);
+  $('.menu').hover(function() {
+    var subMenu = $(this).find('> ul');
+    if ($(this).find('> a').hasClass('collapsed')) {
+      subMenu.fadeIn();
+    } else {
+      subMenu.fadeOut();
+    }
+    $(this).find('> a').toggleClass('collapsed').toggleClass('expanded');
+  });
+});
 
 
 
